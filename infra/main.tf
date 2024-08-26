e9624f38258d28b5308336653e0f967f9e031990
kazu <64774307+kazu-2020@users.noreply.github.com>
2024-08-26 18:57:41 +0900

    OIDC を利用してリソース作成が行えるのを確認

---
+++ infra/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  cloud {
+    organization = "matazou_organization"
+
+    workspaces {
+      name = "oidc-aws-sample"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.56.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
